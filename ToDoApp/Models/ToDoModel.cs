5e9fb9f9247419526ee72c7eebd98c19a51b8e2f
Elena Ksenofontova <lena.ksenofontova@mail.ru>
2023-02-28 18:05:04 +0300

    make class ToDoModel

---
+++ ToDoApp/Models/ToDoModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApp.Models
+{
+    internal class ToDoModel
+    {
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+
+        private bool _isDone;
+        private string _task;
+
+        public bool IsDone
+        {
+            get { return _isDone; }
+            set { _isDone = value; }
+        }
+
+        public string Task
+        {
+            get { return _task; }
+            set { _task = value; }
+        }
+        
+    }
+}
